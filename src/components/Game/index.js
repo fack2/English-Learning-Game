0ab5c5557b47f4e8479fb2bcdbb648318744bfd7
someyaaltous <someyaaltous1995@gmail.com>
2019-08-21 12:26:50 +0300

    complete game component

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -7,14 +7,20 @@ import "./index.css";
 class Game extends Component {
   state = {
     name: "Cherry",
-    image: "https://www.khodarji.com/riyadh/ar/media/catalog/product/cache/4/thumbnail/9df78eab33525d08d6e5fb8d27136e95/c/h/cherry-01.jpg",
+    image:
+      "https://www.khodarji.com/riyadh/ar/media/catalog/product/cache/4/thumbnail/9df78eab33525d08d6e5fb8d27136e95/c/h/cherry-01.jpg",
     score: 0
   };
 
-  changeWord(vocabularies) {
+  changeWord(vocabularies, name) {
     const randomItem = randomPicker(vocabularies);
+
+    this.setState({ name: randomItem.name });
     this.setState(prevState => {
-      return { score: prevState.score + 3, name: randomItem.name };
+      console.log(name);
+      if (this.state.name == name) {
+        return { score: prevState.score + 3, name: randomItem.name };
+      }
     });
   }
 
@@ -25,20 +31,18 @@ class Game extends Component {
         <h2>score {this.state.score}</h2>
 
         {
-          (this.vocabulariesMap = vocabularies.map(
-            ({ name , image }) => (
-              <button class="imageButton">
-                <a
-                  href="#"
-                  onClick={() => {
-                    this.changeWord(vocabularies);
-                  }}
-                >
-                  <Card class="images" image={image} name={name} />
-                </a>
-              </button>
-            )
-          ))
+          (this.vocabulariesMap = vocabularies.map(({ name, image }) => (
+            <button class="imageButton">
+              <a
+                href="#"
+                onClick={() => {
+                  this.changeWord(vocabularies, name);
+                }}
+              >
+                <Card class="images" image={image} name={name} />
+              </a>
+            </button>
+          )))
         }
       </div>
     );
