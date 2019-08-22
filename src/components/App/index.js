33a906f5a10b6da086501c95a3ccc516313b57ad
Sahar Adem <sahar.adem95@gmail.com>
2019-08-22 11:22:28 +0300

    creating first page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,25 @@
-import React from 'react';
+import React , {Component}from 'react';
 import Game from '../Game'
+import FirstPage from '../FirstPage'
+class App extends Component  {
 
-export const App = () => <Game />
+    state = {
+        gameStarted : false
+      };
+
+       startGame = ( ) => {
+           
+        this.setState({gameStarted: true})}
+       render() {
+      return (
+        <div>
+            
+        { (!this.state.gameStarted  )? (<FirstPage startGame={this.startGame} />) : (<Game />)}
+        </div>
+  
+       ) 
+      }
+      
+}
+
+export default App
--- src/components/FirstPage/index.js
+++ src/components/FirstPage/index.js
@@ -1,10 +1,13 @@
 import React, {component} from 'react'
-const FirstPage = ()=>{
+const FirstPage = ({startGame})=>{
+
     return(
         <div>
         <h1>English Learning Game</h1>
-        <button>start</button>
+        <button onClick={() => startGame()
+        }>start</button>
         </div>
     )
 }
+// 
 export default FirstPage
